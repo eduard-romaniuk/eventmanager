a2e09395e0aeedb93822b318b075732497318ea0
Maria Beyrak <maria.beyrak@gmail.com>
2018-04-20 02:16:59 +0300

    Creating mapper for User and updating UsersRepository

--- src/main/java/com/example/eventmanager/dao/UsersRepository.java
+++ src/main/java/com/example/eventmanager/dao/UsersRepository.java
@@ -1,29 +1,45 @@
 package com.example.eventmanager.dao;
 
-
 import com.example.eventmanager.domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.stream.Collectors;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class UsersRepository implements CrudRepository<User> {
 
     private JdbcTemplate jdbcTemplate = JdbcTemplateSingleton.getInstance();
 
+    public class UserMapper implements RowMapper<User> {
+        public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            User user = new User();
+
+            //TODO Add other fields
+            user.setId(resultSet.getLong("id"));
+            user.setUsername(resultSet.getString("username"));
+            user.setPassword(resultSet.getString("password"));
+
+            return user;
+        }
+    }
+
+    //TODO Move to separate file
+    final private static String FIND_BY_USERNAME_SQL = "SELECT id, username, password FROM users WHERE username = ?";
+    final private static String FIND_ONE_SQL = "SELECT username, password FROM users WHERE id = ?";
+    final private static String FIND_ALL_SQL = "SELECT id, username, password FROM users";
+    final private static String UPDATE_USER_SQL = "UPDATE users SET username = ?, password = ? WHERE id = ?";
+    final private static String DELETE_USER_SQL = "DELETE FROM users WHERE id = ?";
+    final private static String SAVE_USER_SQL = "INSERT INTO users (username, password) VALUES (?,?)";
+
     public User findByUsername(String username) {
         try {
-            return (User) jdbcTemplate.queryForObject(
-                    "SELECT id, username, password FROM \"users\" WHERE username = ?",
-                    new Object[]{username},
-                    (RowMapper<Object>) (rs, rowNum) -> new User(
-                            rs.getLong("id"),
-                            rs.getString("username"),
-                            rs.getString("password")));
+            return jdbcTemplate.queryForObject(FIND_BY_USERNAME_SQL, new UserMapper(), username);
         } catch (EmptyResultDataAccessException e) {
+            //TODO Logging
             return null;
         }
     }
@@ -31,51 +47,32 @@ public class UsersRepository implements CrudRepository<User> {
     @Override
     public User findOne(Long id) {
         try {
-            return (User) jdbcTemplate.queryForObject(
-                    "SELECT username, password FROM \"users\" WHERE id = ?",
-                    new Object[]{id},
-                    (RowMapper<Object>) (rs, rowNum) -> new User(
-                            id,
-                            rs.getString("username"),
-                            rs.getString("password")));
+            return jdbcTemplate.queryForObject(FIND_ONE_SQL, new UserMapper(), id);
         } catch (EmptyResultDataAccessException e) {
+            //TODO Logging
             return null;
         }
     }
 
     @Override
     public Iterable<User> findAll() {
-        return jdbcTemplate
-                .queryForList("SELECT id, username, password FROM \"users\"")
-                .stream()
-                .map(row -> new User(
-                        new Long((Integer) row.get("id")),
-                        (String) row.get("username"),
-                        (String) row.get("password")))
-                .collect(Collectors.toList());
+        return jdbcTemplate.query(FIND_ALL_SQL, new UserMapper());
     }
 
     @Override
     public void update(User user) {
-        delete(user);
-        save(user);
+        jdbcTemplate.update(UPDATE_USER_SQL, user.getUsername(), user.getPassword(), user.getId());
     }
 
     @Override
     public void delete(User user) {
-        jdbcTemplate.update(
-                "DELETE FROM \"users\" WHERE id = ?",
-                user.getId()
-        );
+        jdbcTemplate.update(DELETE_USER_SQL, user.getId());
     }
 
     @Override
     public void save(User user) {
         if (user.getId() == null || !this.exists(user.getId())) {
-            jdbcTemplate.update(
-                    "INSERT INTO \"users\" (username, password) VALUES (?, ?)",
-                    user.getUsername(),
-                    user.getPassword());
+            jdbcTemplate.update(SAVE_USER_SQL, user.getUsername(), user.getPassword());
         } else {
             update(user);
         }
