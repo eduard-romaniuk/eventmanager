9413a4e5906909b90f786ad06a5788816fc670a0
vladimir <vladimmyr@gmail.com>
2018-05-10 19:16:11 +0300

    Refactor EditEvent: add Map,Image,Validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -42,7 +42,6 @@ import { ExportEventsPlanComponent } from './components/export-events-plan/expor
 import { UserEditPasswordComponent } from './components/user/user-edit-password/user-edit-password.component';
 import { PersonalPlanSettingComponent } from './components/personal-plan-setting/personal-plan-setting.component';
 import { PersonalPanSettingService } from "./services/personal-pan-setting.service";
-import { EventEditImageComponent } from './components/event-edit-image/event-edit-image.component';
 import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
 import { RecoverLoginComponent } from './components/recover-login/recover-login.component';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
@@ -96,7 +95,6 @@ export class AddressInterceptor implements HttpInterceptor {
     ChatComponent,
     ExportEventsPlanComponent,
     PersonalPlanSettingComponent,
-    EventEditImageComponent,
     RecoverLoginComponent,
     RecoverPasswordComponent,
     ChangePasswordComponent,
