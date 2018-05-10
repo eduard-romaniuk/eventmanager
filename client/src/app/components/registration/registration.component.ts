df3dad91f3bf1c9b86120006fe5a58779f256a32
Eduard romanyuk1990 <eduard.romanyuk1990@gmail.com>
2018-05-10 22:03:37 +0300

    Rollback of auth view

--- client/src/app/components/registration/registration.component.ts
+++ client/src/app/components/registration/registration.component.ts
@@ -4,11 +4,13 @@ import { UserService } from '../../services/user.service';
 import { ToastService } from '../../services/toast.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { JQueryStatic } from 'jquery';
 import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { User } from '../../model/user';
 import { passConfirm, boolean } from '../../utils/validation-tools';
 import 'rxjs/add/operator/map';
+import { JQueryStatic } from 'jquery';
+
+declare var $:JQueryStatic;
 
 @Component({
   selector: 'app-registration',
@@ -29,7 +31,7 @@ export class RegistrationComponent implements OnInit {
     this.form = this.formBuilder.group({
       name: ['', [ Validators.required ]],
       surName: ['', [ Validators.required ]],
-      sex: [null, [ boolean ]],
+      sex: ['', [ boolean ]],
       email: ['', [ Validators.required, Validators.email ], this.emailExists.bind(this)],
       login: ['', [ Validators.required,
         Validators.pattern('^[a-zA-Z0-9_]*$'),
@@ -49,7 +51,7 @@ export class RegistrationComponent implements OnInit {
     this.error = false;
     this.auth.registration(this.user).subscribe(response => {
       this.loading = false;
-      this.router.navigateByUrl('/login');
+      $('#logTab').tab('show');
       this.toast.success('Account registered');
     }, error => {
       this.error = true;
